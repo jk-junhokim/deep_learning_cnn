a862db506e5b51e14bb5e37e732c66956f885fb4
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2022-10-14 13:27:45 +0000

    show mnist data

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 # import matplotlib.pyplot as plt
 # from matplotlib.image import imread
 # from activation_functions.activation_functions import *
-from check_mnist import *
+# from check_mnist import *
+from show_mnist import *
 
 
 
