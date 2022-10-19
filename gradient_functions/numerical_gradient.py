bd728c048a9dbd46daa4a2ce618faf77fd1d78ea
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2022-10-19 14:00:20 +0000

    added modified numerical gradient function

--- gradient_functions/numerical_gradient.py
+++ gradient_functions/numerical_gradient.py
@@ -3,7 +3,7 @@ import numpy as np
 def function_2(x):
     return x[0]**2 + x[1]**2    
 
-def numerical_gradient(f, x):
+def numerical_gradient_1d(f, x):
     h = 1e-4 # 0.0001
     grad = np.zeros_like(x) # intialize gradient values
 
@@ -20,6 +20,27 @@ def numerical_gradient(f, x):
         grad[idx] = (fxh1 - fxh2) / (2 * h)
         x[idx] = tmp_val
 
+    return grad
+
+### NUMERICAL GRADIENT FOR MULTI DIMENSIONAL MATRICES ###
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 값 복원
+        it.iternext()   
+        
     return grad
 
 # test1 = numerical_gradient(function_2, np.array([3.0, 4.0]))
