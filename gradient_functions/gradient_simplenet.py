f4d42ab0ae194e360260b5cc5fd1d79ba3e7c079
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2022-10-19 13:57:23 +0000

    need to modify numerical gradient function so it takes in multi dimensional matrices

--- gradient_functions/gradient_simplenet.py
+++ gradient_functions/gradient_simplenet.py
@@ -13,7 +13,7 @@ from a uniform distribution over [0, 1)
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.rand(2, 3)
+        self.W = np.random.rand(2, 3) # 2 x 3 matrix weight variables
 
     def predict(self, x):
         return np.dot(x, self.W)
@@ -21,7 +21,7 @@ class simpleNet:
     def loss(self, x, t):
         z = self.predict(x)
         y = softmax(z)
-        loss = cross_entropy_error(y, t)
+        loss = cross_entropy_error(y, t) # calculates the error between answer and prediction
 
         return loss
 
@@ -43,24 +43,20 @@ for i in range(3):
     print("")
     
 
-
-"""    
-net = simpleNet()
-print(net.W) # initialized weights
-x = np.array([0.6, 0.9])
-p = net.predict(x)
-print(p)
-print(np.argmax(p))
-t = np.array([0, 0, 1]) # answer label
-loss = net.loss(x, t)
-print(loss)
+"""
+Numerical Gradient Function returns the slope (differentiation)
+at the given point of net.W
+"""
+net_ex = simpleNet()
+weights = net.W
+input_x = np.array([0.6, 0.9])
+answer_t = np.array([0, 0, 1])
 
 def f(W):
     return net.loss(x, t)
 
 dW = numerical_gradient(f, net.W)
 print(dW)
-"""
 
 # f = lambda w: net.loss(x, t)
 # dW = numerical_gradient(f, net.W)
