0e210cc7a733ce588bdd30026684416424800856
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2022-10-27 13:56:42 +0000

    implemented gradient check function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from networks import gradient_check
 
 
 
---
+++ networks/gradient_check.py
@@ -0,0 +1,21 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from mnist_dataset.mnist import load_mnist
+from networks.two_layer_net import *
+
+# read data
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize = True, one_hot_label = True)
+
+network = TwoLayerNet(input_size = 784, hidden_size = 50, output_size = 10)
+
+x_batch = x_train[:3]
+t_batch = t_train[:3]
+
+grad_numerical = network.numerical_gradient(x_batch, t_batch)
+grad_backpropagation = network.gradient(x_batch, t_batch)
+
+# difference between grad_numerical method & grad_backpropagation method
+for key in grad_numerical.keys():
+    diff = np.average(np.abs(grad_backprop[key] - grad_numerical[key]))
+    print(key + ":" + str(diff))
