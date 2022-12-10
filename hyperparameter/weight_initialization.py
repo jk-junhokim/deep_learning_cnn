52ec40fb87f04f2cab5ed45b899be6370b380d59
Junho Kim <junho4623@gmail.com>
2022-12-11 02:21:30 +0900

    automization done!

--- hyperparameter/weight_initialization.py
+++ hyperparameter/weight_initialization.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-### Activation Functions ###
+### ACTIVATION FUNCTIONS ###
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
@@ -13,39 +14,40 @@ def ReLU(x):
 def tanh(x):
     return np.tanh(x)
     
-### Activation Output per Weight Distribution ###
-input_data = np.random.randn(1000, 100)
-node_num = 100  # node for hidden layer
-hidden_layer_size = 5
-activations = {}
+### ACTIVATION LAYER DISTRIBUTION PER WEIGHT INITIALIZATION ###
 
+node_num = 100  # node per hidden layer
+weight_initialization_var = [1, 0.01, np.sqrt(1.0 / node_num), np.sqrt(2.0 / node_num)]
+activation_func = [sigmoid, ReLU, tanh]
+input_data = np.random.randn(1000, 100)
+hidden_layer_size = 5
+
+activations = {}
 x = input_data
 
-for i in range(hidden_layer_size):
-    if i != 0:
-        x = activations[i-1]
+def draw_histogram(activations):
+    # draw histogram
+    for i, a in activations.items():
+        plt.subplot(1, len(activations), i+1)
+        plt.title(str(i+1) + "-layer")
+        if i != 0: plt.yticks([], [])
+        # plt.xlim(0.1, 1)
+        # plt.ylim(0, 7000)
+        plt.hist(a.flatten(), 30, range=(0,1))
+    plt.show()
 
-    # different types of initial values
-    w = np.random.randn(node_num, node_num) * 1
-    # w = np.random.randn(node_num, node_num) * 0.01
-    # w = np.random.randn(node_num, node_num) * np.sqrt(1.0 / node_num)
-    # w = np.random.randn(node_num, node_num) * np.sqrt(2.0 / node_num)
 
-    a = np.dot(x, w)
+for i in range(len(weight_initialization_var)):
+    for j in range(len(activation_func)):
+        activations = {}
+        for k in range(hidden_layer_size):
+            if k != 0:
+                x = activations[k-1]
 
-    # different activation functions
-    z = sigmoid(a)
-    # z = ReLU(a)
-    # z = tanh(a)
+            w = np.random.randn(node_num, node_num) * weight_initialization_var[i]
+            a = np.dot(x, w)
+            z = activation_func[j](a)
 
-    activations[i] = z
-
-# 히스토그램 그리기
-for i, a in activations.items():
-    plt.subplot(1, len(activations), i+1)
-    plt.title(str(i+1) + "-layer")
-    if i != 0: plt.yticks([], [])
-    # plt.xlim(0.1, 1)
-    # plt.ylim(0, 7000)
-    plt.hist(a.flatten(), 30, range=(0,1))
-plt.show()
+            activations[k] = z
+        
+        draw_histogram(activations)
